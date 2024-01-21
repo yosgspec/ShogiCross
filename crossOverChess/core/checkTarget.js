bc02e6b8d239e2004547a3092e268e1dddd2b02c
YOS G-spec <yos.g.spec@gmail.com>
2024-01-21 23:09:14 +0900

    直線移動処理の修正/整理

--- crossOverChess/core/checkTarget.js
+++ crossOverChess/core/checkTarget.js
@@ -18,18 +18,24 @@ function checkTarget(field, piece, pX, pY){
 
 	// 移動範囲文字の親子関係
 	const targetChars = [
-		["A", ["a"]],
-		["B", ["b"]],
-		["C", ["c"]],
-		["D", ["d"]],
-		["E", ["a", "e"]],
-		["F", ["a", "f"]],
-		["G", ["b", "g"]],
-		["H", ["b", "h"]],
-		["I", ["c", "i"]],
-		["J", ["c", "j"]],
-		["K", ["d", "k"]],
-		["L", ["d", "l"]]
+		["A", {child: ["a"]}],
+		["B", {child: ["b"]}],
+		["C", {child: ["c"]}],
+		["D", {child: ["d"]}],
+		["E", {child: ["a", "e"]}],
+		["F", {child: ["a", "f"]}],
+		["G", {child: ["b", "g"]}],
+		["H", {child: ["b", "h"]}],
+		["I", {child: ["c", "i"]}],
+		["J", {child: ["c", "j"]}],
+		["K", {child: ["d", "k"]}],
+		["L", {child: ["d", "l"]}]
+	];
+
+	// 直線移動範囲文字のジャンプ回数
+	const linerChars = [
+		["*", {jmps: 0}],
+		["+", {jmps: 1}]
 	];
 
 	/** rangeの原点座標を取得
@@ -92,7 +98,7 @@ function checkTarget(field, piece, pX, pY){
 			const [oX, oY] = getRange0(rng);
 
 			// 通常移動
-			for(const [parent, child] of targetChars){
+			for(const [parent, {child}] of targetChars){
 				for(let rY=0;rY<rng.length;rY++){
 					for(let rX=0;rX<rng[rY].length;rX++){
 						const [x, y] = [rX+pX-oX, rY+pY-oY];
@@ -104,37 +110,25 @@ function checkTarget(field, piece, pX, pY){
 					}
 				}
 			}
-			// 直線移動
-			for(let rY=oY-1;rY<=oY+1;rY++){
-				for(let rX=oX-1;rX<=oX+1;rX++){
-					if(rX === oX && rY === oY || rng[rY][rX] !== "*") continue;
-					const [incX, incY] = [rX-oX, rY-oY];
-					for(let x=pX,y=pY;;){
-						x+=incX;
-						y+=incY;
-						if(!canMove(false, x, y) && !canMove(isAttack, x, y)) break;
-						field[y][x].isTarget = true;
-						if(field[y][x].piece) break;
-					}
-				}
 
-				let jmpCnt = 0;
-				for(let rX=oX-1;rX<=oX+1;rX++){
-					if(rX === oX && rY === oY || rng[rY][rX] !== "+") continue;
-					const [incX, incY] = [rX-oX, rY-oY];
-					for(let x=pX,y=pY;;){
-						x+=incX;
-						y+=incY;
-						if(!canMove(false, x, y, false) && !canMove(isAttack, x, y, !!jmpCnt)) break;
-						if(0 < jmpCnt){
-							field[y][x].isTarget = true;
-						}
-						if(field[y][x].piece){
-							if(0 === jmpCnt){
-								jmpCnt++;
+			// 直線移動
+			for(const [char, {jmps}] of linerChars){
+				for(let rY=oY-1;rY<=oY+1;rY++){
+					for(let rX=oX-1;rX<=oX+1;rX++){
+						let jmpCnt = jmps;
+						if(rX === oX && rY === oY || rng[rY][rX] !== char) continue;
+						const [incX, incY] = [rX-oX, rY-oY];
+						for(let x=pX,y=pY;;){
+							x+=incX;
+							y+=incY;
+							const isJumped = 0 === jmpCnt;
+							if(!canMove(false, x, y) && !canMove(isAttack, x, y, isJumped)) break;
+							if(isJumped) field[y][x].isTarget = true;
+							if(field[y][x].piece){
+								if(isJumped) break;
+								jmpCnt--;
 								continue;
 							}
-							else break;
 						}
 					}
 				}
