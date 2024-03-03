38bdae2c4358c15e545599a13b27437be115b806
YOS G-spec <yos.g.spec@gmail.com>
2024-03-03 22:18:55 +0900

    参照設定を修正

--- shogiCross/ShogiCross/core/json.js
+++ shogiCross/ShogiCross/core/json.js
@@ -1,8 +1,9 @@
 import canvasFont from "../json/canvasFont.json" assert {type: "json"};
+import gameSoft from "../json/gameSoft.json" assert {type: "json"};
 import games from "../json/games.json" assert {type: "json"};
 import boards from "../json/boards.json" assert {type: "json"};
 import panels from "../json/panels.json" assert {type: "json"};
 import pieces from "../json/pieces.json" assert {type: "json"};
 import pieceRange from "../json/pieceRange.json" assert {type: "json"};
 import pieceCost from "../json/pieceCost.json" assert {type: "json"};
-export {canvasFont, games, boards, panels, pieces, pieceRange, pieceCost};
+export {canvasFont, gameSoft, games, boards, panels, pieces, pieceRange, pieceCost};
--- shogiCross/ShogiCross/lib.js
+++ shogiCross/ShogiCross/lib.js
@@ -1,14 +1,5 @@
 import {canvasFont} from "./core/canvasFontLoader.js";
-import {Board} from "./core/board.js"
-import {Piece} from "./core/piece.js"
-import boards from "./json/boards.json" assert {type: "json"};
-import games from "./json/games.json" assert {type: "json"};
-import gameSoft from "./json/gameSoft.json" assert {type: "json"};
-export {
-	canvasFont,
-	Board,
-	Piece,
-	boards,
-	games,
-	gameSoft
-};
+import {Board} from "./core/board.js";
+import {Piece} from "./core/piece.js";
+import {gameSoft, games, boards} from "./core/json.js";
+export {canvasFont, Board, Piece,gameSoft, games, boards};
