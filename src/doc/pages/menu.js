71eeb3bb840a142ea2528c0ce04e1cd87411f5b7
YOS G-spec <yos.g.spec@gmail.com>
2024-03-22 19:26:50 +0900

    vanilla.jsでドキュメントを作成する準備

---
+++ src/doc/pages/menu.js
@@ -0,0 +1,33 @@
+export default `
+<h3>ボード操作</h3>
+<div><a href="#board">ボードを作成する</a></div>
+<div><a href="#gameSoft">ゲームプリセットを指定</a></div>
+<div><a href="#playBoard">ボードタイプの変更</a></div>
+<div><a href="#useStand">駒台の使用有無</a></div>
+<div><a href="#players">ゲームの参加人数</a></div>
+<div><a href="#playPieces">ボードの初期配置</a></div>
+<div><a href="#canvasFit">キャンバスの寸法調整</a></div>
+<div><a href="#boardSizes">ボードの寸法を変更</a></div>
+<div><a href="#backGroundColor">背景色を変更</a></div>
+<div><a href="#useRankSize">駒の格で大きさを変更</a></div>
+<div><a href="#isDrawShadow">駒の影の描写を変更</a></div>
+<div><a href="#autoDrawing">描写の自動更新を変更</a></div>
+<div><a href="#onDrawed">描写時に任意の処理を行う</a></div>
+<div><a href="#onGameOver">ゲームオーバー時に任意の処理を行う</a></div>
+<div><a href="#freeMode">自由移動の有効化</a></div>
+<div><a href="#downloadImage">スクリーンショット</a></div>
+<div><a href="#rotate">ボードの回転</a></div>
+<div><a href="#close">ボードを破棄</a></div>
+
+<h3>駒の操作</h3>
+<div><a href="#putNewPiece">駒を任意のマスに配置する</a></div>
+<div><a href="#putStartPieces">駒の初期配置を行う</a></div>
+<div><a href="#setTextPieces">駒をテキスト形式で配置する</a></div>
+<div><a href="#getTextPieces">駒の配置をテキスト形式で取得</a></div>
+
+<h3>棋譜の操作</h3>
+<div><a href="#getTextRecord">棋譜の取得</a></div>
+<div><a href="#getJsonRecord">棋譜データの取得</a></div>
+<div><a href="#setJsonRecord">棋譜データの反映</a></div>
+<div><a href="#switchRecord">手を進める・戻す</a></div>
+`;
