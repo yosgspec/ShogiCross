7aec48e101e9adb352f51f6d020a26af11f7d41e
YOS G-spec <yos.g.spec@gmail.com>
2024-03-30 16:34:26 +0900

    サンプルコード表示用のcodeViewer修正

--- src/iframe/codeViewer.js
+++ src/iframe/codeViewer.js
@@ -1,12 +1,4 @@
 {
-	const main = document.querySelector("main");
-	main.style.display = "flex";
-	const sub = main.firstChild;
-	sub.style.width = "50svw";
-	sub.style.height = "94svh";
-	const canvas = sub.querySelector("#shogiCross");
-	canvas.style.maxWidth = "50svw";
-
 	const $ = document.createElement("textarea");
 	$.style.width="100%";
 	$.style.fontSize="14px";
@@ -15,5 +7,13 @@
 	$.wrap="off";
 	$.readonly="true";
 	$.textContent=document.body.innerHTML.split(/\r|\n|\r\n/).slice(0,-1).join("\n");
+
+	const main = document.querySelector("main");
+	main.style.display = "flex";
+	const sub = main.firstChild;
+	sub.style.width = "50svw";
+	sub.style.height = "94svh";
+	const canvas = sub.querySelector("#shogiCross");
+	canvas.style.maxWidth = "50svw";
 	main.appendChild($);
 }
