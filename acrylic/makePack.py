d90cfb31fe645bcbe13b650972717bae79e7b793
YOS G-spec <yos.g.spec@gmail.com>
2025-01-06 18:13:07 +0900

    アクリルデータ作成用プログラムを修正

--- acrylic/makePack.py
+++ acrylic/makePack.py
@@ -1,9 +1,14 @@
+import csv
+import glob
+import shutil
+from os import path, makedirs
 import xml.etree.ElementTree as ET
-import os
+
 
 # 設定
-input_dir = "./games/svg/00_MAX"  # SVGファイルのフォルダ
-output_file = "MAX"  # 出力ファイルの接頭辞
+src_path = "svg"  # 入力ファイルのディレクトリ
+input_csv = "paperPieceCount.csv"  # 入力するCSVファイル
+game_name = "MAX"  # SVGを作成するゲーム名
 viewport = 3000  # ビューポートサイズ
 ratio = 0.0410  # スケール比
 delta_x = ratio * 2670  # SVGの幅（px）
@@ -12,17 +17,41 @@ offset_x = (viewport * ratio - delta_x) / 2
 offset_y = ratio * -350
 max_cols = 7  # 1行に配置するSVGの数
 max_rows = 8  # 出力ファイルごとの最大行数
-is_mirror = True
-is_swap = True
+is_mirror = True  # 裏面を左右反転するか?
+is_swap = True  # 駒の配置順を前後交互にするか?
+
+# 入力ファイル一覧
+input_files_side = {
+	"A": [],
+	"B": []
+}
+
+# 入力ファイルリスト作成
+with open(input_csv, mode="r", encoding="utf-8") as file:
+	datas = list(csv.reader(file))
+	DATA_ROW = 4
+	DATA_COL = 3
+
+	for i in range(DATA_ROW):
+		games = datas.pop(0)[DATA_COL:]
+
+	for row in datas:
+		piece = "".join(row[:2])
+		row = row[DATA_COL:]
+		for i,pcs in enumerate(row):
+			if game_name != games[i]: continue
+			for side in input_files_side:
+				img = glob.glob(f"{src_path}/*{piece}{side}.svg")[0]
+				for j in range(int(pcs)):
+					input_files_side[side].append(img)
+			break
 
 # 総行数計算
-num_files = len([f for f in os.listdir(os.path.join(input_dir, "0")) if f.endswith(".svg")])
+num_files = len(input_files_side["A"])
 total_rows = (num_files + max_cols - 1) // max_cols
 
 # 処理
-for side in range(2):
-	side_path = os.path.join(input_dir, str(side))
-	input_files = sorted([f for f in os.listdir(side_path) if f.endswith(".svg")])
+for side, input_files in input_files_side.items():
 	if is_swap:
 		input_files, unswap_files = [], input_files
 		result = []
@@ -55,7 +84,7 @@ for side in range(2):
 		x, y = 0, 0
 		current_row = start_row
 		for index, filename in enumerate(input_files[start_row * max_cols:end_row * max_cols]):
-			tree = ET.parse(os.path.join(side_path, filename))
+			tree = ET.parse(filename)
 			root = tree.getroot()
 
 			# グループ作成
@@ -63,7 +92,7 @@ for side in range(2):
 				f"translate({x-offset_x}, {y-offset_y}) scale({ratio})"
 				if (current_row + index) % 2 == 0 else
 				f"translate({x+offset_x+delta_x}, {y+offset_y+delta_y}) scale({ratio}) scale(-1, -1)"
-			) if side == 0 else (
+			) if side == "A" else (
 				(
 					f"translate({x+offset_x+delta_x}, {y-offset_y}) scale({ratio}) scale(-1, 1)"
 					if (current_row + index) % 2 == 0 else
@@ -87,6 +116,6 @@ for side in range(2):
 
 		# ファイル保存
 		tree = ET.ElementTree(svg_root)
-		tree.write(f"{output_file}{side}_{file_id}.svg")
+		tree.write(f"{game_name}{side}_{file_id}.svg")
 
 print("Completed.")
