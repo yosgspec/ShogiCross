7d486dbb45fee2d074a4a379b8271d214821f225
YOS G-spec <yos.g.spec@gmail.com>
2025-08-18 23:49:34 +0900

    ビルド設定更新

--- src/ShogiCross/core/data.js
+++ src/ShogiCross/core/data.js
@@ -32,10 +32,7 @@
 /**
  * @typedef {Object} BoardInitOption - ボードの初期化オプション
  * @prop {string} playBoard - ボードタイプ
- * @prop {PlayerOption[]} PlayerInitOption - プレイヤーの初期化設定
- * @prop {string} playerOptions.gameName - ゲーム名(基準となる駒の配置セット)
- * @prop {string} playerOptions.pieceSet - 駒の配置パターン
- * @prop {string} playerOptions.cpuEngine - CPUエンジン名
+ * @prop {PlayerInitOption[]} playerOptions - プレイヤーの初期化設定
  * @prop {2|4} players - プレイヤー人数(2 or 4)
  * @prop {boolean} useStand - 駒台の使用有無
  * @prop {number} canvasWidth - Canvas幅
