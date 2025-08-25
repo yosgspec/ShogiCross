52f84daac1f1cae855213a2aa8eb69dd28e26c4c
YOS G-spec <yos.g.spec@gmail.com>
2025-08-26 03:57:23 +0900

    ドキュメント更新

--- src/menu.js
+++ src/menu.js
@@ -23,42 +23,42 @@ export default `
 <div><a href="https://github.com/yosgspec/ShogiCross/tree/main/doc/pieces">駒一覧</a></div>
 
 <h3>ボード設定</h3>
-<div><a href="#doc/board">ボードを構成する</a></div>
-<div><a href="#doc/gameSoft">ゲームをプリセットから起動</a></div>
-<div><a href="#doc/playBoard">ボードタイプの変更</a></div>
-<div><a href="#doc/useStand">駒台の使用有無</a></div>
-<div><a href="#doc/playerOptions">プレイヤー設定/駒の初期配置</a></div>
-<div><a href="#doc/players">ゲームの参加人数</a></div>
-<div><a href="#doc/canvasFit">Canvasの寸法調整</a></div>
-<div><a href="#doc/boardSizes">ボードの寸法を変更</a></div>
-<div><a href="#doc/backgroundColor">背景色を変更</a></div>
-<div><a href="#doc/useRankSize">駒の格で大きさを変更しない</a></div>
-<div><a href="#doc/isDrawShadow">駒の影の描写を変更</a></div>
-<div><a href="#doc/autoDrawing">描写の自動更新を変更</a></div>
-<div><a href="#doc/moveModeFree">自由移動の有効化</a></div>
-<div><a href="#doc/moveModeViewOnly">移動を無効化</a></div>
-<div><a href="#doc/onDrawed">描写時に任意の処理を行う</a></div>
-<div><a href="#doc/onGameOver">プレイヤーが敗北した時の処理を行う</a></div>
-<div><a href="#doc/useUIControl">操作パネル表示を変更する</a></div>
-<div><a href="#doc/makeUIControl">操作パネル要素を取得</a></div>
-<div><a href="#doc/downloadImage">スクリーンショットを保存する</a></div>
-<div><a href="#doc/rotate">盤面を回転する</a></div>
+<div><a href="#doc/board/board">ボードを構成する</a></div>
+<div><a href="#doc/boardOption/gameSoft">ゲームをプリセットから起動</a></div>
+<div><a href="#doc/boardOption/playBoard">ボードタイプの変更</a></div>
+<div><a href="#doc/boardOption/useStand">駒台の使用有無</a></div>
+<div><a href="#doc/boardOption/playersOption">プレイヤー設定/駒の初期配置</a></div>
+<div><a href="#doc/boardOption/players">ゲームの参加人数</a></div>
+<div><a href="#doc/boardOption/canvasFit">Canvasの寸法調整</a></div>
+<div><a href="#doc/boardOption/boardSizes">ボードの寸法を変更</a></div>
+<div><a href="#doc/boardOption/backgroundColor">背景色を変更</a></div>
+<div><a href="#doc/boardOption/useRankSize">駒の格で大きさを変更しない</a></div>
+<div><a href="#doc/boardOption/isDrawShadow">駒の影の描写を変更</a></div>
+<div><a href="#doc/boardOption/autoDrawing">描写の自動更新を変更</a></div>
+<div><a href="#doc/boardOption/moveModeFree">自由移動の有効化</a></div>
+<div><a href="#doc/boardOption/moveModeViewOnly">移動を無効化</a></div>
+<div><a href="#doc/board/onDrawed">描写時に任意の処理を行う</a></div>
+<div><a href="#doc/board/onGameOver">プレイヤーが敗北した時の処理を行う</a></div>
+<div><a href="#doc/boardOption/useUIControl">操作パネル表示を変更する</a></div>
+<div><a href="#doc/board/makeUIControl">操作パネル要素を取得</a></div>
+<div><a href="#doc/board/downloadImage">スクリーンショットを保存する</a></div>
+<div><a href="#doc/board/rotate">盤面を回転する</a></div>
 <div><a href="#doc/extendData">ゲームデータを拡張する</a></div>
-<div><a href="#doc/close">ゲームを終了する</a></div>
+<div><a href="#doc/board/close">ゲームを終了する</a></div>
 
 <h3>駒の操作</h3>
-<div><a href="#doc/putNewPiece">駒を任意のマスに配置する</a></div>
-<div><a href="#doc/putStartPieces">駒の初期配置(メソッド)</a></div>
-<div><a href="#doc/setTextPieces">駒をテキスト形式で配置する</a></div>
-<div><a href="#doc/getTextPieces">駒の配置をテキスト形式で取得</a></div>
-<div><a href="#doc/record2String">局面の記録を文字列に変換</a></div>
+<div><a href="#doc/board/putNewPiece">駒を任意のマスに配置する</a></div>
+<div><a href="#doc/board/putStartPieces">駒の初期配置(メソッド)</a></div>
+<div><a href="#doc/board/setTextPieces">駒をテキスト形式で配置する</a></div>
+<div><a href="#doc/board/getTextPieces">駒の配置をテキスト形式で取得</a></div>
+<div><a href="#doc/record/getText">局面の記録を文字列に変換</a></div>
 
 <h3>棋譜の操作</h3>
-<div><a href="#doc/getTextAll">棋譜の取得</a></div>
-<div><a href="#doc/getJson">棋譜データを取得</a></div>
-<div><a href="#doc/setJson">棋譜データを反映</a></div>
-<div><a href="#doc/switch">手を進める・戻す</a></div>
-<div><a href="#doc/move">手数を移動する</a></div>
+<div><a href="#doc/record/getTextAll">棋譜の取得</a></div>
+<div><a href="#doc/record/getJson">棋譜データを取得</a></div>
+<div><a href="#doc/record/setJson">棋譜データを反映</a></div>
+<div><a href="#doc/record/switch">手を進める・戻す</a></div>
+<div><a href="#doc/record/move">手数を移動する</a></div>
 
 <h3>ゲームデータ仕様</h3>
 <div><a href="#md/data/gameSoft">ゲーム用プリセット</a></div>
